eb5c06f93352189a616b7274e50feca9ec37e8e9
Shadow192 <mihaildragos18@yahoo.com>
2020-01-16 16:33:19 +0200

    BW Solo/Doubles

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ cd /home/container
 # Output Current Java Version
 java -version
 
+rm -rf Hollow1/ Hollow2/ Hollow3/ Hollow4/ Hollow5/ Lighthouse1/ Lighthouse2/ Lighthouse3/ Lighthouse4/ Lighthouse5/ Pernicious1/ Pernicious2/ Pernicious3/ Pernicious4/ Pernicious5/ Rooftop1/ Rooftop2/ Rooftop3/ Rooftop4/ Rooftop5/ Waterfall1/ Waterfall2/ Waterfall3/ Waterfall4/ Waterfall5/
+tar -xf bw.tar.gz
+
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
