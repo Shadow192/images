1daf09ecb645266bd8764b77086944387412b578
Shadow192 <mihaildragos18@yahoo.com>
2020-01-26 00:21:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ java -version
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+rm -rf Arenas/ Arenas_1/ Arenas_2/
+unzip arene.zip
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
